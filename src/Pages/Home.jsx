1471180b0bf74cc1be708884789b2466853b7198
yagi17 <nafissadik785@gmail.com>
2024-08-17 17:32:12 +0600

    Fix: Pagenation Position

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -219,115 +219,129 @@ const Home = () => {
         </div>
 
         {/* item section */}
-        <div>
-          {paginatedProducts.length > 0 ? <>{}</> : <></>}
-          <div className="grid md:grid-cols-3 gap-4 w-full">
-            {paginatedProducts.map((product, index) => (
-              <div key={index}>
-                <article className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden">
-                  <div>
-                    <img
-                      className="object-cover h-64 w-full"
-                      src={product.image}
-                      alt={product.model}
-                    />
-                  </div>
+        <div className="w-full">
+          {paginatedProducts.length > 0 ? (
+            <>
+              <div className="grid md:grid-cols-3 gap-4 w-full">
+                {paginatedProducts.map((product, index) => (
+                  <div key={index}>
+                    <article className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden">
+                      <div>
+                        <img
+                          className="object-cover h-64 w-full"
+                          src={product.image}
+                          alt={product.model}
+                        />
+                      </div>
 
-                  <div className="flex flex-col gap-1 mt-4 px-4">
-                    <h2 className="text-lg font-semibold text-red-500">
-                      {product.name}
-                    </h2>
-                    <span className="font-normal">{product.color} Variant</span>
-                    <span className="font-bold">${product.price}</span>
-                  </div>
+                      <div className="flex flex-col gap-1 mt-4 px-4">
+                        <h2 className="text-lg font-semibold text-red-500">
+                          {product.name}
+                        </h2>
+                        <span className="font-normal">
+                          {product.color} Variant
+                        </span>
+                        <span className="font-bold">${product.price}</span>
+                      </div>
 
-                  <div className="flex justify-between p-4 text-[11px]">
-                    <div className="flex flex-col w-fit">
-                      {product.features.map((feature, index) => (
-                        <div key={index}>
-                          {index <= 2 && (
-                            <div className="flex items-center">
-                              <GoDotFill />
-                              {feature}
+                      <div className="flex justify-between p-4 text-[11px]">
+                        <div className="flex flex-col w-fit">
+                          {product.features.map((feature, index) => (
+                            <div key={index}>
+                              {index <= 2 && (
+                                <div className="flex items-center">
+                                  <GoDotFill />
+                                  {feature}
+                                </div>
+                              )}
                             </div>
-                          )}
+                          ))}
                         </div>
-                      ))}
-                    </div>
-                    <Link>
-                      <button className="btn bg-red-600 text-white hover:bg-red-600">
-                        View Details
-                      </button>
-                    </Link>
+                        <Link>
+                          <button className="btn bg-red-600 text-white hover:bg-red-600">
+                            View Details
+                          </button>
+                        </Link>
+                      </div>
+                    </article>
                   </div>
-                </article>
+                ))}
               </div>
-            ))}
-          </div>
 
-          {/* Pagination */}
-          <div className="mt-8 flex justify-center border w-full items-center">
-            <div className="inline-flex border-2 border-gray-500 rounded-lg justify-center items-center p-1 bg-white space-x-2">
-              <button
-                className={`p-1 rounded border text-black bg-white hover:text-white hover:bg-blue-600 hover:border-blue-600 ${
-                  currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
-                }`}
-                onClick={() =>
-                  setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
-                }
-                disabled={currentPage === 1}
-              >
-                <svg
-                  className="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-                  />
-                </svg>
-              </button>
-              <p className="text-red-600">
-                Page {currentPage} of{" "}
-                {Math.ceil(filteredProducts.length / itemsPerPage)}
-              </p>
+              {/* Pagination */}
 
-              <button
-                className={`p-1 rounded border text-black bg-white hover:text-white hover:bg-blue-600 hover:border-blue-600 ${
-                  currentPage ===
-                  Math.ceil(filteredProducts.length / itemsPerPage)
-                    ? "opacity-50 cursor-not-allowed"
-                    : ""
-                }`}
-                onClick={() =>
-                  setCurrentPage((prevPage) =>
-                    Math.min(
-                      prevPage + 1,
+              <div className="mt-8 flex justify-center w-full items-center">
+                <div className="inline-flex border-2 border-gray-500 rounded-lg justify-center items-center p-1 bg-white space-x-2">
+                  <button
+                    className={`p-1 rounded border text-black bg-white hover:text-white hover:bg-blue-600 hover:border-blue-600 ${
+                      currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                    onClick={() =>
+                      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
+                    }
+                    disabled={currentPage === 1}
+                  >
+                    <svg
+                      className="w-5 h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                      />
+                    </svg>
+                  </button>
+                  <p className="text-red-600">
+                    Page {currentPage} of{" "}
+                    {Math.ceil(filteredProducts.length / itemsPerPage)}
+                  </p>
+
+                  <button
+                    className={`p-1 rounded border text-black bg-white hover:text-white hover:bg-blue-600 hover:border-blue-600 ${
+                      currentPage ===
                       Math.ceil(filteredProducts.length / itemsPerPage)
-                    )
-                  )
-                }
-                disabled={
-                  currentPage ===
-                  Math.ceil(filteredProducts.length / itemsPerPage)
-                }
-              >
-                <svg
-                  className="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
+                        ? "opacity-50 cursor-not-allowed"
+                        : ""
+                    }`}
+                    onClick={() =>
+                      setCurrentPage((prevPage) =>
+                        Math.min(
+                          prevPage + 1,
+                          Math.ceil(filteredProducts.length / itemsPerPage)
+                        )
+                      )
+                    }
+                    disabled={
+                      currentPage ===
+                      Math.ceil(filteredProducts.length / itemsPerPage)
+                    }
+                  >
+                    <svg
+                      className="w-5 h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="h-1/2 flex justify-center items-center w-full">
+                <h2 className="text-2xl text-center font-semibold text-red-600">
+                  No car found !!
+                </h2>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
